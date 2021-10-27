935923d4e81d997d5f2273ae0595cd2a0dff1c9c
Jacob Larson <jacob.larson55112@gmail.com>
2021-10-27 12:57:20 -0500

    completed week 1 assignment

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -1,7 +1,3 @@
-let cat = document.getElementById('cat');
-
-cat.addEventListener('click', notPets);
-
-function noPets() {
-  console.log('working')
+function noPetsOnlyLooks(){
+  console.log('No pet the kitty')
 }
