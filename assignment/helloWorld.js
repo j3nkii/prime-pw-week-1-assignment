c9c1029ccae4a39a466ef67318d8a7ad5f9f36ee
Jacob Larson <jacob.larson55112@gmail.com>
2021-10-27 17:07:13 -0500

    pet the  kitty feature

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -1,3 +1,11 @@
-function noPetsOnlyLooks(){
-  console.log('No pet the kitty')
+var pets = 0;
+
+function noPetsOnlyLooks() {
+  pets++
+
+  if (pets <= 1) {
+      console.log('You\'ve pet the kitty ' + pets + ' time!')
+  } else  {
+    console.log('You\'ve pet the kitty ' + pets + ' times!')
+  }
 }
